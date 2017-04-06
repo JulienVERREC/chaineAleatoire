740af9f8815281339acdbdf1ba53f98019d29c21
JulienVERREC <julien.verrecchia@orange.fr>
2017-04-06 17:24:10 +0200

    add dynamic array in html, add coordonate function , radio buttons

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
     function randomString (nombrecaracteres) {
 
-    	var result = " ";
+    	var resultat = " ";
 		var alphabet = "abcdefghijklmnopqrstuvwxyz";
 		var round = 0;
 
@@ -11,11 +11,11 @@ $(document).ready(function(){
     		round = Math.floor(Math.random() * alphabet.length  );
     		alphabet.charAt();
 
-    		result+=alphabet.charAt(round);
+    		resultat+=alphabet.charAt(round);
 
     	}
 
-    	return result;
+    	return resultat;
 
 
     }
@@ -31,7 +31,7 @@ $(document).ready(function(){
 
     	};
 
-		console.log( maChaineAleatoire );
+		//console.log( maChaineAleatoire );
 
 
     function randomArray ( absisses, ordonnees ) {
@@ -42,9 +42,9 @@ $(document).ready(function(){
     						
 	    	for (var i = 0; i < absisses; i++) {
     			for (var j = 0; j < ordonnees ; j++) {
-    				ligne.push(randomString(5));
+    				ligne.push(randomString(10));
     			}
-	    	   	console.log(ligne);
+	    	   	//console.log(ligne);
 	    		tableau.push(ligne);
 	    		ligne = [];				
 	    	}
@@ -53,15 +53,96 @@ $(document).ready(function(){
 
     }					
 
+    	var monTableau = randomArray(10,10);
 
-    	var monTableau = randomArray(6,6);
-
-		console.log(monTableau);		       	
-    	
-
-
+		//console.log(monTableau);		       	
     
+
+    function htmlarray (monTableau){
+
+    	var result = "<table class = 'table table-bordered'>";
+    	result = result + "<thead>";
+    	result = result + "<tr><th>eureka</th>";
+
+
+    			
+
+    		for (var h = 0 ; h < monTableau.length; h++) {
+    			result = result + " <th><input id = 'x"+ [h] +"' type='radio' name='absisses'></th> ";
+
+    		}
+
+    			result = result + "</tr>"
+
+
+    				result = result + "</thead><tbody>";
+    				result = result + "<tr>";
+    	
+    				for (var i = 0; i < monTableau.length; i++) {
+    					result = result + " <td><input id = 'y" + [i] +"' type ='radio' name='ordonnees'></td> ";
+    			
+    					var change = monTableau[i];
+    					for (var j = 0; j < change.length; j++) {
+    					result = result + "<td id = '"+[i]+[j]+"'>" + change[j] +"</td>";
+    					}
+
+    					result = result + "</tr>";
+    				}
+
+
+    				result = result + "</tbody></table>";
+
+    				return result;
+
+    }
+    				var monTableauhtml = htmlarray(monTableau);
+    			    console.log(monTableauhtml);
+    	
+    			/*	var tablalpha=[];
+
+    				var idalpha= $('td').attr('id');
+    				tablalpha.push(idalpha);
+    					
+    				console.log(idalpha);
+    				console.log(tablalpha);*/
+
+    	var tableaucoordonnees = [];
+
+    function coordonnees (absisses, ordonnees){
+
+    	var absisses = $("input[name=absisses]:checked").attr("id");
+    	var ordonnees = $("input[name=ordonnees]:checked").attr("id");
+
+    	tableaucoordonnees.push(absisses,ordonnees);
+
+    	console.log(tableaucoordonnees);
+    
+    }
+
+
+    		var tablalpha = [];
+
+			var tableauHtml=htmlarray(monTableau);
+			$('#emplacement').append(tableauHtml);
+
+    		//console.log(tableauHtml);
+
+    		$("#action").click(function(){
+
+    			
+    		coordonnees();
+
+    			if (tableaucoordonnees ) {
+
+    			}
+
+
+
+
+
+    		});
+
+
+
+
 });
-
-
-
